3948b98c89b31ef48a30bfb25238a9fc9c3e7d7d
omardeveloping <omar.montanares@inacapmail.cl>
2025-12-26 20:17:00 -0300

    + apiview de register

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,8 @@
 from dj_rest_auth.views import LoginView
 from dj_rest_auth.registration.views import RegisterView
 from rest_framework import status
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -13,13 +13,16 @@ from .serializers import (
 )
 
 
-class CustomRegisterAPIView(APIView):
+class CustomRegisterAPIView(GenericAPIView):
     """
     Registration endpoint that returns JWT access/refresh (email + passwords only).
+    Uses GenericAPIView so DRF browsable API renders a form.
     """
 
+    serializer_class = CustomRegisterSerializer
+
     def post(self, request, *args, **kwargs):
-        serializer = CustomRegisterSerializer(data=request.data)
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save(request)
 
