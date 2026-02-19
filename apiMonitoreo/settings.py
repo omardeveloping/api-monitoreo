27aa455073d6730b399ab89480397e9be3510c01
omardeveloping <omar.montanares@inacapmail.cl>
2026-02-19 19:18:17 -0300

    +tiempo de timeout incrementado

--- apiMonitoreo/settings.py
+++ apiMonitoreo/settings.py
@@ -235,8 +235,8 @@ CELERY_TASK_ROUTES = {
 }
 CELERY_TASK_ANNOTATIONS = {
     "dashboard.tasks.importar_videos_mdvr_task": {
-        "soft_time_limit": int(os.environ.get("MDVR_IMPORT_SOFT_TIME_LIMIT", "7200")),
-        "time_limit": int(os.environ.get("MDVR_IMPORT_TIME_LIMIT", "7800")),
+        "soft_time_limit": int(os.environ.get("MDVR_IMPORT_SOFT_TIME_LIMIT", "21600")),
+        "time_limit": int(os.environ.get("MDVR_IMPORT_TIME_LIMIT", "23400")),
         "rate_limit": os.environ.get("MDVR_IMPORT_RATE_LIMIT", "1/m"),
     }
 }
